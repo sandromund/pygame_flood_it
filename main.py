e2f9866a837689b4ad55289e8114328c09986de0
Sandro <31536101+sandromund@users.noreply.github.com>
2024-01-07 19:25:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import numpy as np
 import pygame
 
 BOARD_SIZE = 18
-N_COLORS = 6
+N_COLORS = 2
 CELL_SIZE = 50
 BACKGROUND_COLOR = (194, 173, 138)
 
 colors = np.random.randint(255, size=(N_COLORS, 3))
 board = np.random.randint(N_COLORS, size=(BOARD_SIZE, BOARD_SIZE))
+connected = np.zeros(shape=(BOARD_SIZE, BOARD_SIZE))
+connected[0][0] = 1
 window_size = CELL_SIZE * BOARD_SIZE
 pygame.init()
 screen = pygame.display.set_mode((window_size, window_size))
@@ -57,6 +59,24 @@ def get_color_of_clicked_rect(mouse_position):
     return x_new, y_new
 
 
+def find_connection():
+    queue = [(0, 0)]
+    while len(queue) > 0:
+        x_i, y_j = queue.pop(0)
+        if board[x_i][y_j] == 1:
+            continue
+        for i, j in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            x_t = x_i + i
+            y_t = y_j + j
+            if 0 <= x_t < BOARD_SIZE and 0 <= y_t < BOARD_SIZE:
+                print(x_t, y_t)
+                if board[x_t][y_t] == board[0][0]:
+                    connected[x_t][y_t] = 1
+                    queue.append((x_t, y_t))
+
+
+find_connection()
+
 running = True
 while running:
     for event in pygame.event.get():
@@ -66,8 +86,10 @@ while running:
         if mouse_buttons[0]:
             x, y = get_color_of_clicked_rect(event.pos)
             print(x, y)
+            print(connected)
     screen.fill(BACKGROUND_COLOR)
     draw_board()
+
     pygame.display.flip()
 
 pygame.quit()
